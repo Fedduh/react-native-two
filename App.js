060ffd993af083359f032de464955f6cf1e02018
Fedde <feddetilma@hotmail.com>
2020-11-21 13:00:59 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, SafeAreaView, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, styles.text]}>
+      <Text style={styles.text}>Hello!</Text>
+    </SafeAreaView>
   );
 }
+// 1.15.51 in video -> modules voor padding android top
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "orange"
   },
+  text: {
+    color: "white"
+  }
 });
