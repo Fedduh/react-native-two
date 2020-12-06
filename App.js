15bc7f6d22c16bb1bcab23a4364ec51013c52def
Fedde <feddetilma@hotmail.com>
2020-12-06 20:18:16 +0100

    app picker

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Switch, Text, TextInput, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
+import AppPicker from "./app/components/AppPicker";
 import Card from "./app/components/Card";
 
+import Screen from "./app/components/Screen";
+
 import AppButton from "./app/components/AppButton";
+import AppTextInput from "./app/components/AppTextInput";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
@@ -14,13 +18,23 @@ import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
-  return <ListingsScreen></ListingsScreen>;
+  const [isNew, setIsNew] = useState(false);
+
+  return (
+    <Screen style={styles.main}>
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="email" />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
   main: {
     padding: 20,
-    marginTop: 20,
-    backgroundColor: "#f8f4f4"
+    marginTop: 20
+  },
+  input: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1
   }
 });
--- app/config/colors.js
+++ app/config/colors.js
@@ -5,6 +5,8 @@ export default {
   tertiary: "#ffe66d", // yellow
   black: "#000",
   white: "#fff",
+  // greys
+  dark: "#0c0c0c",
   medium: "#6e6969",
   light: "#f8f4f4",
   danger: "#ff5252"
